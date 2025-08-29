9a3dd4dad69c1ff606f58986874ecdc69b3581fc
technical-tedxdyp <technical.tedxdyp@gmail.com>
2025-08-29 23:11:39 +0530

    Update main.js

--- src/main.js
+++ src/main.js
@@ -1,8 +1,75 @@
+// import React from "react";
+// import ReactDOM from "react-dom/client";
+// import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+// import { Element } from "react-scroll";
+
+// import Header from "./components/Header";
+// import Home from "./components/Home";
+// import Speakers from "./components/Speakers";
+// import Sponsors from "./components/Sponsors";
+// import Footer from "./components/Footer";
+// import GetTicket from "./components/GetTicket";
+// import SuccessPage from "./components/SuccessPage";
+
+// import "../src/index.css";
+
+// function App() {
+//   return (
+//     <Router>
+//       <Header />   {/* Always visible */}
+//       <Routes>
+//         <Route
+//           path="/"
+//           element={
+//             <>
+//               {/* Wrap sections in <Element> with matching names */}
+//               <Element name="home">
+//                 <Home />
+//               </Element>
+
+//               {/* Uncomment and add Theme component when ready
+//               <Element name="theme">
+//                 <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
+//                   <h1 className="text-4xl">Theme Section</h1>
+//                 </div>
+//               </Element>
+//               */}
+
+//               <Element name="speakers">
+//                 <Speakers />
+//               </Element>
+
+//               <Element name="sponsors">
+//                 <Sponsors />
+//               </Element>
+//             </>
+//           }
+//         />
+//         <Route path="/get-ticket" element={<GetTicket />} />
+//         <Route path="/success" element={<SuccessPage />} />
+//       </Routes>
+//       <Footer />   {/* Always visible */}
+//     </Router>
+//   );
+// }
+
+// // Check if root element exists before rendering
+// const rootElement = document.getElementById("root");
+// if (rootElement) {
+//   ReactDOM.createRoot(rootElement).render(
+//     <React.StrictMode>
+//       <App />
+//     </React.StrictMode>
+//   );
+// } else {
+//   console.error("Root element not found. Make sure your index.html has a div with id='root'");
+// }
+
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import { Element } from "react-scroll";
-
+// REMOVED: import { Element } from "react-scroll";
 import Header from "./components/Header";
 import Home from "./components/Home";
 import Speakers from "./components/Speakers";
@@ -10,7 +77,6 @@ import Sponsors from "./components/Sponsors";
 import Footer from "./components/Footer";
 import GetTicket from "./components/GetTicket";
 import SuccessPage from "./components/SuccessPage";
-
 import "../src/index.css";
 
 function App() {
@@ -22,26 +88,26 @@ function App() {
           path="/"
           element={
             <>
-              {/* Wrap sections in <Element> with matching names */}
-              <Element name="home">
+              {/* CHANGED: Replace Element with div + id for native scrolling */}
+              <div id="home">
                 <Home />
-              </Element>
-
+              </div>
+              
               {/* Uncomment and add Theme component when ready
-              <Element name="theme">
+              <div id="theme">
                 <div className="min-h-screen flex items-center justify-center bg-gray-900 text-white">
                   <h1 className="text-4xl">Theme Section</h1>
                 </div>
-              </Element>
+              </div>
               */}
-
-              <Element name="speakers">
+              
+              <div id="speakers">
                 <Speakers />
-              </Element>
-
-              <Element name="sponsors">
+              </div>
+              
+              <div id="sponsors">
                 <Sponsors />
-              </Element>
+              </div>
             </>
           }
         />
@@ -64,3 +130,4 @@ if (rootElement) {
 } else {
   console.error("Root element not found. Make sure your index.html has a div with id='root'");
 }
+
